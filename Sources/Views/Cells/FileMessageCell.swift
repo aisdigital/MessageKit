55156e0ccd867d631bbc8f7956e45a72496436d3
Luiz Felipe <originaldojames@gmail.com>
2018-10-03 17:36:23 -0300

    Fix constraints constants for document cell"

--- Sources/Views/Cells/FileMessageCell.swift
+++ Sources/Views/Cells/FileMessageCell.swift
@@ -27,7 +27,7 @@ open class FileMessageCell: MessageContentCell {
         fileImageView.translatesAutoresizingMaskIntoConstraints = false
         fileNameLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        let fileImageViewLeftConstraint = fileImageView.leftAnchor.constraint(equalTo: messageContainerView.leftAnchor, constant: 15)
+        let fileImageViewLeftConstraint = fileImageView.leftAnchor.constraint(equalTo: messageContainerView.leftAnchor, constant: messageContainerView.frame.height * 0.1)
         let fileImageViewCenterYConstraint = fileImageView.centerYAnchor.constraint(equalTo: messageContainerView.centerYAnchor)
         let fileImageViewHeightConstraint = fileImageView.heightAnchor.constraint(equalTo: messageContainerView.heightAnchor, multiplier: 0.8)
         let fileImageViewWidthConstraint = fileImageView.widthAnchor.constraint(equalTo: messageContainerView.heightAnchor, multiplier: 0.8)
